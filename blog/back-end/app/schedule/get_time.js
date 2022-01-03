eb16e39c26fd60c18caa6e5229b79aaba4151ee9
hichho <52339006+hichho@users.noreply.github.com>
2022-01-03 10:39:07 +0800

    feat: schedule

---
+++ blog/back-end/app/schedule/get_time.js
@@ -0,0 +1,19 @@
+'use strict';
+const Subscription = require('egg').Subscription;
+
+class GetTime extends Subscription {
+  static get schedule() {
+    return {
+      // interval: '3s',
+      cron: '*/3*****',
+      type: 'worker',
+    };
+  }
+
+  async subscribe() {
+    console.log(Date.now());
+  }
+}
+
+module.exports = GetTime;
+
