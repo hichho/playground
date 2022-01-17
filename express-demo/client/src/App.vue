5382a0ba46e9517ef23c108517e855a6a37eed41
hichho <52339006+hichho@users.noreply.github.com>
2022-01-17 10:30:31 +0800

    feat:add express-demo

---
+++ express-demo/client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>123</h1>
+</template>
+<script>
+
+import axios from 'axios';
+import qs from 'qs';
+
+export default {
+  name: 'App',
+  mounted() {
+    axios.post('/api/getStudents', qs.stringify({
+      type: 'less', score: 99
+    }))
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+  }
+};
+</script>
+<style>
+
+</style>
