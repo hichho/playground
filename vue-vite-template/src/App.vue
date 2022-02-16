aebed56ad208136ba838ca7dc79c65743e365430
hichho <52339006+hichho@users.noreply.github.com>
2022-02-16 13:53:33 +0800

    feat: save...

--- vue-vite-template/src/App.vue
+++ vue-vite-template/src/App.vue
@@ -1,45 +1,56 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from '@/components/HelloWorld.vue';
-import '@/assets/demo.css';
-import styles_1 from '@/assets/test_1.module.css';
-import styles_2 from '@/assets/test_2.module.css';
-import demoData from '@/assets/demoData.json';
-import service from '@/services';
-
-const demoFunc = (str: string): void => {
-  console.log(str);
-}
-demoFunc('i am for test!');
-console.log(demoData);
-service.getData();
+<template>
+  <div>
+    Vue3.0+Typescript
+  </div>
+</template>
+<script lang="ts">
+export default {};
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png"/>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite"/>
-  <h2 :class="styles_1.name">test_1</h2>
-  <h2 :class="styles_2.name">test_2</h2>
-  <h3 class="name">test_3</h3>
-  <h3 class="demo_content">test_4</h3>
-</template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-<style lang="less">
-@import "styles/demo.less";
+//this is test for less or scss';
+<!--<script setup lang="ts">-->
+<!--// This starter template is using Vue 3 <script setup> SFCs-->
+<!--// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup-->
+<!--import HelloWorld from '@/components/HelloWorld.vue';-->
+<!--import '@/assets/demo.css';-->
+<!--import styles_1 from '@/assets/test_1.module.css';-->
+<!--import styles_2 from '@/assets/test_2.module.css';-->
+<!--import demoData from '@/assets/demoData.json';-->
+<!--import service from '@/services';-->
 
-.name {
-  color: green;
-}
-</style>
+<!--const demoFunc = (str: string): void => {-->
+<!--  console.log(str);-->
+<!--}-->
+<!--demoFunc('i am for test!');-->
+<!--console.log(demoData);-->
+<!--service.getData();-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <img alt="Vue logo" src="./assets/logo.png"/>-->
+<!--  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite"/>-->
+<!--  <h2 :class="styles_1.name">test_1</h2>-->
+<!--  <h2 :class="styles_2.name">test_2</h2>-->
+<!--  <h3 class="name">test_3</h3>-->
+<!--  <h3 class="demo_content">test_4</h3>-->
+<!--</template>-->
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
+<!--<style lang="less">-->
+<!--@import "styles/demo.less";-->
+
+<!--.name {-->
+<!--  color: green;-->
+<!--}-->
+<!--</style>-->
 
