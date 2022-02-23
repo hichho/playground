aa2ce195641c72e40f42bd3c6bb0690c8cebbf5f
hichho <52339006+hichho@users.noreply.github.com>
2022-02-23 13:08:06 +0800

    feat:test Virtual list

--- vue2-template/src/main.js
+++ vue2-template/src/main.js
@@ -2,24 +2,25 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import dataV from '@jiaminghi/data-view';
-import ElementUI,{ Notification } from 'element-ui';
+import ElementUI, { Notification } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import scroll from 'vue-seamless-scroll';
 
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(dataV);
 Vue.use(ElementUI);
+Vue.use(scroll);
 Vue.prototype.notify = Notification;
 
-router.beforeEach((to,from,next)=>{
-  if(to.meta.title){
-    document.title=to.meta.title
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
   }
   next();
-})
+});
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
