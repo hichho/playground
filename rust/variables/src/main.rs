0efdc68c3b4773bf523e10d3ddd531a7041da56d
hichho <52339006+hichho@users.noreply.github.com>
2022-03-01 21:06:49 +0800

    feat:save...

---
+++ rust/variables/src/main.rs
@@ -0,0 +1,7 @@
+fn five(x: i32) -> i32 {
+    5 + x
+}
+fn main() {
+    let x = five(10);
+    println!("{}", x);
+}
--- wjw-data-display-platform/util/Common.js
+++
@@ -1,9 +0,0 @@
-const getRandomNum = (value) => {
-  if (!isNaN(Number(value))) {
-    return Number(value) + Math.random();
-  }
-  return 0;
-};
-export {
-  getRandomNum
-};
