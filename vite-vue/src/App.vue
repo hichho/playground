89faa607dae3fe904e3386edcce81f94777eaa6e
hichho <52339006+hichho@users.noreply.github.com>
2022-03-17 16:13:12 +0800

    feat: delete the request

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -1,15 +1,11 @@
 <script lang="ts">
-import {RequestConfig} from 'vue-request';
 
 export default {
-  components: {RequestConfig}
 }
 
 </script>
 <template>
-  <RequestConfig>
     <router-view/>
-  </RequestConfig>
 </template>
 <style lang="less">
 @import "styles/global.less";
