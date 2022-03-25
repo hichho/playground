007e03f2f6f6a38f0765b7a868e4c84189c8cc66
hichho <52339006+hichho@users.noreply.github.com>
2022-03-25 20:35:28 +0800

    feat: adjust the alias

--- vite-react/src/pages/index.tsx
+++ vite-react/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Router, Route} from "react-router";
+import less from '@/style/index.module.less';
 
-const IndexPage = () => {
+const IndexPage = (props: any) => {
     return (
-        <h1>
-            123
-        </h1>
+        <div className={less.frame}>
+            <h1
+                onClick={() => props.history.push('/home')}>index</h1>
+        </div>
     )
 }
 export default IndexPage;
--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from "path";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: resolve(__dirname, "./src"),
-      },
-    ],
-  },
-  server:{
-    proxy:{
-      "/app": "http://zcse-index.tmp.kepai365.ltd/",
-      "/upload": "http://zcse-index.tmp.kepai365.ltd/"
-    }
-  },
-  base:'./',
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            less: {
+                //配置全局less
+                additionalData: `@import "${resolve(__dirname, 'src/theme.module.less')}";`,
+            }
+        }
+    },
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: resolve(__dirname, "./src"),
+            },
+        ],
+    },
+    server: {
+        proxy: {
+            "/app": "http://zcse-index.tmp.kepai365.ltd/",
+            "/upload": "http://zcse-index.tmp.kepai365.ltd/"
+        }
+    },
+    base: './',
 })
