0602353ed2dbc96b34b22e63ff36abd8397b727d
hichho <52339006+hichho@users.noreply.github.com>
2022-04-18 21:10:09 +0800

    feat:add error handler

--- nest-two/src/coffees/coffees.service.ts
+++ nest-two/src/coffees/coffees.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -23,7 +28,12 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((item) => item.id === +id);
+    // throw 'A random error';
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
