4169c815b4facbffc03a6fb59a2c12b27a9c1761
hichho <52339006+hichho@users.noreply.github.com>
2022-04-18 18:18:58 +0800

    feat:1、add get,post request 2、add body from nest

--- nest-two/src/coffees/coffees.controller.ts
+++ nest-two/src/coffees/coffees.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get('/flavors')
   findAll() {
-    return 'This action return s all coffees';
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  // findOne(@Param() params) {
+  //   return `this action returns #${params.id} coffee`;
+  // }
+  findOne(@Param('id') id: string) {
+    return `this action returns #${id} coffee`;
+  }
+
+  @Post('create')
+  create(@Body() body) {
+    return body;
   }
 }
