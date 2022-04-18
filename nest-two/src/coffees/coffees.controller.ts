cd8aa96c3999af5bd96c29674d6914e972ef6ec7
hichho <52339006+hichho@users.noreply.github.com>
2022-04-18 20:12:59 +0800

    feat:add all types of http request

--- nest-two/src/coffees/coffees.controller.ts
+++ nest-two/src/coffees/coffees.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('/flavors')
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Query() paginationQuery, @Res() response) {
+    const { limit, offset } = paginationQuery;
+    response
+      .status(200)
+      .send(
+        `this action returns all coffees.limit:${limit} , offset:${offset}`,
+      );
   }
 
   @Get(':id')
@@ -16,7 +33,18 @@ export class CoffeesController {
   }
 
   @Post('create')
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action update #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action remove #${id} coffee`;
   }
 }
