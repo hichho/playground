77d5563931adcedd2be8e9a64823d2c64b1821d6
hichho <52339006+hichho@users.noreply.github.com>
2022-04-20 11:25:28 +0800

    feat:add typeOrm and postgres

--- nest-two/src/app.module.ts
+++ nest-two/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      //自动加载模块
+      autoLoadEntities: true,
+      //在typeOrm同步数据库，在production环境钟禁用
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
