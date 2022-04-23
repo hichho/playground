a1dee19f800a676ca9bc030eb2f9e37cc3eb143d
hichho <52339006+hichho@users.noreply.github.com>
2022-04-23 16:00:33 +0800

    pref:delete the old nest-project

--- nest-project/src/http.service.ts
+++
@@ -1,7 +0,0 @@
-import { Inject, Injectable, Optional } from "@nestjs/common"
-
-@Injectable()
-export class HttpService<T> {
-  constructor(@Optional() @Inject("HTTP_OPTIONS") private readonly httpClient: T) {
-  }
-}
