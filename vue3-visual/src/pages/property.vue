8360e6c52da5b4138d2c29cf9256d0c9f30f06b4
hichho <52339006+hichho@users.noreply.github.com>
2022-07-27 09:54:47 +0800

    feat:adjust the propertyPage

---
+++ vue3-visual/src/pages/property.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>123</div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'PropertyPage',
+  });
+</script>
+<style></style>
